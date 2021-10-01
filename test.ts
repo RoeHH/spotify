8d2150cd15d05d4e5d50a676b0a397e337176748
RoeHH <github.r@12i.ch>
2021-10-01 18:58:07 +0200

    :)

--- test.ts
+++ test.ts
@@ -1,5 +1,15 @@
 import { parse } from "https://deno.land/std@0.100.0/flags/mod.ts";
 import { Application } from "https://deno.land/x/abc@v1.3.1/mod.ts";
+import "https://deno.land/x/dotenv@v3.0.0/load.ts";
+import { Logger } from "https://deno.land/x/loggaby@1.1.3/mod.ts";
+
+const logger = new Logger(true);
+
+const clientSecret = Deno.env.get("clientSecret");
+if (!clientSecret) {
+  logger.error('No "clientSecret" enviroment variable found');
+  Deno.exit();
+}
 
 const app = new Application();
 
@@ -11,8 +21,9 @@ const PORT = argPort ? Number(argPort) : DEFAULT_PORT;
 console.log(`Listening on Port: ${PORT}`);
 console.log(`http://localhost:${PORT}/`);
 
-const my_client_id = "084bf4f1acfc46d8b31e9d3783c52e37";
-const redirect_uri = "https://sleepy-taiga-91048.herokuapp.com/i";
+const myClientId = "084bf4f1acfc46d8b31e9d3783c52e37";
+const redirectUri = "https://sleepy-taiga-91048.herokuapp.com/i";
+let code;
 
 app
   .get("/", (res) => {
@@ -21,15 +32,28 @@ app
       "https://accounts.spotify.com/authorize" +
         "?response_type=code" +
         "&client_id=" +
-        my_client_id +
+        myClientId +
         (scopes ? "&scope=" + encodeURIComponent(scopes) : "") +
         "&redirect_uri=" +
-        encodeURIComponent(redirect_uri)
+        encodeURIComponent(redirectUri)
     );
   })
-  .get("/i", (c) => {
+  .get("/i", async (c) => {
     const { code } = c.queryParams as { code: string };
-    console.log(code);
-    return "code";
+    const response = await fetch(`https://accounts.spotify.com/api/token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        grant_type: "authorization_code",
+        code: code,
+        redirectUri: redirectUri,
+        client_id: myClientId,
+        client_secret: clientSecret,
+      }),
+    }).then((response) => response.json());
+    console.log(response)
+    return response;
   })
   .start({ port: PORT });
