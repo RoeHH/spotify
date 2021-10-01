daf1000bc4dcaf5a42e28a913c0b296ad4b4e038
RoeHH <github.r@12i.ch>
2021-10-01 19:13:57 +0200

    :)

--- test.ts
+++ test.ts
@@ -36,7 +36,7 @@ app
       "https://accounts.spotify.com/authorize" +
         "?response_type=code" +
         "&client_id=" +
-        ClientId +
+        clientId +
         (scopes ? "&scope=" + encodeURIComponent(scopes) : "") +
         "&redirect_uri=" +
         encodeURIComponent(redirectUri)
